390752804b6d43035e77a0900797aee31f5e7030
mr.funkster <mr.funksters@gmail.com>
2021-01-27 15:07:56 +0200

    added a jquery smooth scroll and header visibility function

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,36 @@
-let burgerBtn = document.querySelector('.header-burger');
-let overlay = document.querySelector('.overlay');
-let navMenu = document.querySelector('.menu');
-let wrapper = document.querySelector('.wrapper');
+let burgerBtn            = document.querySelector('.header-burger');
+let overlay              = document.querySelector('.overlay');
+let navMenu              = document.querySelector('.menu');
+let wrapper              = document.querySelector('.wrapper');
+let mobileHeader         = document.querySelector('.mobile-header');
+let welcomeSection       = document.querySelector('.welcome-section')
+let mobileHeaderHeight   = mobileHeader.offsetHeight;
+let welcomeSectionHeight = welcomeSection.offsetHeight;
+let viewportWidth        = window.innerWidth;
+let playBtn              = document.querySelector('.player-btn');
+
+const closeBurger = () => {
+    burgerBtn.classList.remove('show-burger');
+    overlay.classList.remove('show');
+    wrapper.classList.remove('show-menu');
+    setTimeout(()=>{
+        document.body.classList.remove('lock');
+    }, 350)    
+}
+
+const headerVisibility = () => {
+    if ((window.pageYOffset >= (welcomeSectionHeight - mobileHeaderHeight)) && viewportWidth <= 1024) {
+        mobileHeader.classList.add('visible')
+    } else {
+        mobileHeader.classList.remove('visible')
+    }
+}
+
+window.addEventListener('resize', () => {
+    viewportWidth = window.innerWidth;
+    headerVisibility();
+});
+window.addEventListener("scroll", headerVisibility);
 
 burgerBtn.addEventListener('click', () => {
     if(burgerBtn.classList.contains('show-burger')) {
@@ -21,15 +50,31 @@ burgerBtn.addEventListener('click', () => {
                 closeBurger();
             }
         });
-    }
-})
+    };
+});
 
-const closeBurger = () => {
-    burgerBtn.classList.remove('show-burger');
-    overlay.classList.remove('show');
-    wrapper.classList.remove('show-menu');
-    setTimeout(()=>{
-        document.body.classList.remove('lock');
-    }, 350)    
+// Scroll
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        anchors.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        const blockID = anchor.getAttribute('href').substr(1);
+        let block = document.getElementById(blockID);
+        let headerOffset;
+        if (window.innerWidth <= 1024) {
+            headerOffset = mobileHeaderHeight;
+        } else {
+            headerOffset = 0;
+        };
+        closeBurger();
+        setTimeout(() => {
+            $('html, body').animate({
+                scrollTop: $(block).offset().top - headerOffset
+            }, 800);
+        }, 200)
+    })
 }
-
