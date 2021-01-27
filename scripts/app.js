4847f850b9c52345176b13695dcb2a39a14d9ae8
mr.funkster <mr.funksters@gmail.com>
2021-01-27 22:22:04 +0200

    fixed resize method

--- scripts/app.js
+++ scripts/app.js
@@ -32,8 +32,6 @@ const headerVisibility = () => {
 }
 
 window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
     viewportWidth = window.innerWidth;
     headerVisibility();
 });
