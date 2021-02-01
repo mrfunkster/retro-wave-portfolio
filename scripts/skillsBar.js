c053b373defaf8d9a99d16a08ca31eb1218f693a
mr.funkster <mr.funksters@gmail.com>
2021-02-01 22:11:49 +0200

    added a bottles

--- scripts/skillsBar.js
+++ scripts/skillsBar.js
@@ -2,6 +2,9 @@ let typingParagraph = document.querySelector('.text-typing');
 let dialogueBody    = document.querySelector('.dialogue-section');
 let skillBarSection = document.querySelector('.skills-section');
 let bartenderMouth  = document.querySelector('.bartender-mouth');
+let bottles         = document.querySelectorAll('.bottle');
+let bottlesContainer = document.querySelector('.bottles');
+let letterItterator = 0;
 let isDone = false;
 let index = 0;
 let charCount = 0;
@@ -11,34 +14,70 @@ let letter = '';
 
 let textArray = [
     "Hello and Welcome to my Skills Bar! I Will make for you a my best skill-coctails tonight. If You interested in it, just show me bottle, that you want to drink!",
+    "My First Skii",
+    "My Second Skii",
+    "My Third Skii",
+    "My Fourth Skii",
+    "My Fifth Skii",
+    "My Sixth Skii",
+    "My Seventh Skii",
+    "My Eighth Skii",
 ]
 
 function textTyping(index) {
     dialogueBody.classList.add('visible');
     currentText = textArray[index];
+    let letterItterator = 0;
     setTimeout(() => {
         bartenderMouth.classList.add('speaking');
-        let i = 0;
         function typing() {
             setTimeout(() => {
-                letter = currentText.slice(0, ++i);
+                letter = currentText.slice(0, ++letterItterator);
                 typingParagraph.innerHTML = letter;
-                if(i <= currentText.length) {
+                if(letterItterator <= currentText.length) {
                     typing();
                 } else {
                     bartenderMouth.classList.remove('speaking');
+                    if(!isDone) {
+                        addListenerToBottles();
+                        bottlesContainer.classList.add('active');
+                        isDone = true;
+                    }
                 }
-            }, 100);
+            }, 75);
         };
         typing();
     }, 100)
 };
 
+
+
 function startTyping() {
     if(window.scrollY >= skillBarSection.offsetTop && !isDone) {
         textTyping(0);
-        isDone = true;
     }
 }
 
+function addListenerToBottles() {
+    for(let i = 0; i < bottles.length; i++) {
+        bottles[i].classList.add('active');
+        bottles[i].addEventListener('click', () => {
+            clickOnBottle(i);
+        });
+    }
+}
+
+function clickOnBottle(bottle) {
+    clearDialogue();
+    setTimeout(() => {
+        textTyping(bottle + 1);
+    }, 100)
+}
+
+function clearDialogue() {
+    bartenderMouth.classList.remove('speaking');
+    currentText = '';
+}
+
 window.addEventListener('scroll', startTyping)
+
